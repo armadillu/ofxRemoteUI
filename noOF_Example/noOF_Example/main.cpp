1cffe3afda8779676e41f0a4d951b88693376e4a
Oriol Ferrer Mesià <oriol@uri.cat>
2013-06-30 00:39:52 +0200

    hansi fixed uriencode

--- noOF_Example/noOF_Example/main.cpp
+++ noOF_Example/noOF_Example/main.cpp
@@ -55,5 +55,5 @@ void update(){
 
 	prevMyParam = myParam;
 	prevColor = color;
-
 }
+
--- src/uriencode.h
+++ src/uriencode.h
@@ -10,36 +10,85 @@
 #ifndef ofxRemoteUIClientOSX_urlencode_h
 #define ofxRemoteUIClientOSX_urlencode_h
 
-// hansi: hextable from 
+// hansi: hextable from
 // http://stackoverflow.com/questions/10324/how-can-i-convert-a-hexadecimal-number-to-base-10-efficiently-in-c
-static const long HEX2DEC[] = {
+/*static const long HEX2DEC[] = {
 	[0 ... 255] = -1, // bit aligned access into this table is considerably
 	['0'] = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, // faster for most modern processors,
 	['A'] = 10, 11, 12, 13, 14, 15,       // for the space conscious, reduce to
 	['a'] = 10, 11, 12, 13, 14, 15        // signed char.
+};*/
+const char HEX2DEC[256] =
+{
+    /*       0  1  2  3   4  5  6  7   8  9  A  B   C  D  E  F */
+    /* 0 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* 1 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* 2 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* 3 */  0, 1, 2, 3,  4, 5, 6, 7,  8, 9,-1,-1, -1,-1,-1,-1,
+
+    /* 4 */ -1,10,11,12, 13,14,15,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* 5 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* 6 */ -1,10,11,12, 13,14,15,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* 7 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* 8 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* 9 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* A */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* B */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* C */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* D */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* E */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    /* F */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1
 };
 
 // http://en.wikipedia.org/wiki/Percent-encoding#Types_of_URI_characters
-static bool SAFE[256] = {
+/*static bool SAFE[256] = {
 	[0 ... 255] = false,
 	['a'...'z'] = true,
 	['A'...'Z'] = true,
 	['0'...'9'] = true,
 	['.'] = true
+};*/
+
+// Only alphanum is safe.
+const char SAFE[256] =
+{
+    /*      0 1 2 3  4 5 6 7  8 9 A B  C D E F */
+    /* 0 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* 1 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* 2 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,1,0,
+    /* 3 */ 1,1,1,1, 1,1,1,1, 1,1,0,0, 0,0,0,0,
+
+    /* 4 */ 0,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1,
+    /* 5 */ 1,1,1,1, 1,1,1,1, 1,1,1,0, 0,0,0,0,
+    /* 6 */ 0,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1,
+    /* 7 */ 1,1,1,1, 1,1,1,1, 1,1,1,0, 0,0,0,0,
+
+    /* 8 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* 9 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* A */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* B */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+
+    /* C */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* D */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* E */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* F */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0
 };
+
 
 std::string UriEncode(const std::string & sSrc);
 std::string UriEncode(const std::string & sSrc)
 {
-	
-	
+
+
 	const char DEC2HEX[16 + 1] = "0123456789ABCDEF";
 	const unsigned char * pSrc = (const unsigned char *)sSrc.c_str();
 	const int SRC_LEN = sSrc.length();
 	unsigned char * const pStart = new unsigned char[SRC_LEN * 3];
 	unsigned char * pEnd = pStart;
 	const unsigned char * const SRC_END = pSrc + SRC_LEN;
-	
+
 	for (; pSrc < SRC_END; ++pSrc)
 	{
 		if (SAFE[*pSrc])
@@ -52,29 +101,29 @@ std::string UriEncode(const std::string & sSrc)
 			*pEnd++ = DEC2HEX[*pSrc & 0x0F];
 		}
 	}
-	
+
 	std::string sResult((char *)pStart, (char *)pEnd);
 	delete [] pStart;
 	return sResult;
 }
 
 
-std::string UriDecode(const std::string & sSrc); 
+std::string UriDecode(const std::string & sSrc);
 std::string UriDecode(const std::string & sSrc)
 {
 	// Note from RFC1630: "Sequences which start with a percent
 	// sign but are not followed by two hexadecimal characters
 	// (0-9, A-F) are reserved for future extension"
-	
+
 	const unsigned char * pSrc = (const unsigned char *)sSrc.c_str();
 	const int SRC_LEN = sSrc.length();
 	const unsigned char * const SRC_END = pSrc + SRC_LEN;
 	// last decodable '%'
 	const unsigned char * const SRC_LAST_DEC = SRC_END - 2;
-	
+
 	char * const pStart = new char[SRC_LEN];
 	char * pEnd = pStart;
-	
+
 	while (pSrc < SRC_LAST_DEC)
 	{
 		if (*pSrc == '%')
@@ -88,14 +137,14 @@ std::string UriDecode(const std::string & sSrc)
 				continue;
 			}
 		}
-		
+
 		*pEnd++ = *pSrc++;
 	}
-	
+
 	// the last 2- chars
 	while (pSrc < SRC_END)
 		*pEnd++ = *pSrc++;
-	
+
 	std::string sResult(pStart, pEnd);
 	delete [] pStart;
 	return sResult;
