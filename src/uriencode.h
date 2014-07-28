3010d3e2eeb6e012cd8835749b551cebef2bb343
Oriol Ferrer Mesià <oriol@localprojects.net>
2014-07-28 12:54:17 -0400

    split up uriencode in -.cpp and .h (sorry)

--- src/uriencode.h
+++ src/uriencode.h
@@ -10,6 +10,8 @@
 #ifndef urlencode_h
 #define urlencode_h
 
+#include <iostream.h>
+
 // hansi: hextable from
 // http://stackoverflow.com/questions/10324/how-can-i-convert-a-hexadecimal-number-to-base-10-efficiently-in-c
 /*static const long HEX2DEC[] = {
@@ -78,76 +80,6 @@ const char SAFE[256] =
 
 
 std::string UriEncode(const std::string & sSrc);
-std::string UriEncode(const std::string & sSrc)
-{
-
-
-	const char DEC2HEX[16 + 1] = "0123456789ABCDEF";
-	const unsigned char * pSrc = (const unsigned char *)sSrc.c_str();
-	const int SRC_LEN = (int) sSrc.length();
-	unsigned char * const pStart = new unsigned char[SRC_LEN * 3];
-	unsigned char * pEnd = pStart;
-	const unsigned char * const SRC_END = pSrc + SRC_LEN;
-
-	for (; pSrc < SRC_END; ++pSrc)
-	{
-		if (SAFE[*pSrc])
-			*pEnd++ = *pSrc;
-		else
-		{
-			// escape this char
-			*pEnd++ = '%';
-			*pEnd++ = DEC2HEX[*pSrc >> 4];
-			*pEnd++ = DEC2HEX[*pSrc & 0x0F];
-		}
-	}
-
-	std::string sResult((char *)pStart, (char *)pEnd);
-	delete [] pStart;
-	return sResult;
-}
-
-
 std::string UriDecode(const std::string & sSrc);
-std::string UriDecode(const std::string & sSrc)
-{
-	// Note from RFC1630: "Sequences which start with a percent
-	// sign but are not followed by two hexadecimal characters
-	// (0-9, A-F) are reserved for future extension"
-
-	const unsigned char * pSrc = (const unsigned char *)sSrc.c_str();
-	const int SRC_LEN = (int)sSrc.length();
-	const unsigned char * const SRC_END = pSrc + SRC_LEN;
-	// last decodable '%'
-	const unsigned char * const SRC_LAST_DEC = SRC_END - 2;
-
-	char * const pStart = new char[SRC_LEN];
-	char * pEnd = pStart;
-
-	while (pSrc < SRC_LAST_DEC)
-	{
-		if (*pSrc == '%')
-		{
-			char dec1, dec2;
-			if (-1 != (dec1 = HEX2DEC[*(pSrc + 1)])
-				&& -1 != (dec2 = HEX2DEC[*(pSrc + 2)]))
-			{
-				*pEnd++ = (dec1 << 4) + dec2;
-				pSrc += 3;
-				continue;
-			}
-		}
-
-		*pEnd++ = *pSrc++;
-	}
-
-	// the last 2- chars
-	while (pSrc < SRC_END)
-		*pEnd++ = *pSrc++;
-
-	std::string sResult(pStart, pEnd);
-	delete [] pStart;
-	return sResult;
-}
 
 #endif
